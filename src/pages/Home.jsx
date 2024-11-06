bda13ae0813628a2c2b5abe4a12630daa056ffa4
rawisut06 <tungtung0685@gmail.com>
2024-11-06 10:46:56 +0700

    changed sector using  onClick button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,38 @@
-
+import axios from "axios"
+import { useEffect, useState } from "react"
+import Admin from "./Admin";
+import User from "./User"
 
 function Home() {
+  const [sector, setSector] = useState("")
+  const [employees, setEmployees] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get("https://jsd5-mock-backend.onrender.com/members");
+      setEmployees(response.data);
+    }
+    getData()
+  })
+
+  const sectorComponent = () => {
+    if (sector === "Admin") return <Admin />;
+    if (sector === "User") return <User />
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <div className="text-center text-2xl font-bold">
+        <h1>Generation Thailand</h1>
+        <h1>
+        {sector === "" ? "React - Assignment" : (
+            `Home - ${sector} Sector`
+          )}
+        </h1>
+      </div>
+      <button onClick={() => setSector("User")}>User Home Sector</button>
+      <button onClick={() => setSector("Admin")}>Admin Home Sector</button>
+    </div>
   )
 }
 
