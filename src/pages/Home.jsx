a70974397df14382ec7aea4780782f1099e0d381
rawisut06 <tungtung0685@gmail.com>
2024-11-06 11:04:15 +0700

    updated sector button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
 
   return (
     <div>
-      <div className="text-center text-2xl font-bold">
+      <div className="text-center text-4xl font-bold m-8">
         <h1>Generation Thailand</h1>
         <h1>
         {sector === "" ? "React - Assignment" : (
@@ -30,8 +30,20 @@ function Home() {
           )}
         </h1>
       </div>
-      <button onClick={() => setSector("User")}>User Home Sector</button>
-      <button onClick={() => setSector("Admin")}>Admin Home Sector</button>
+      <div className="flex p-4 gap-4 justify-center m-8">
+        <button
+          className={`p-4 rounded-xl hover:bg-slate-900 hover:text-white ${sector === "User" ? "bg-slate-900 text-white" : "bg-slate-300"}`}
+          onClick={() => setSector("User")}
+        >
+          User Home Sector
+        </button>
+        <button
+          className={`p-4 rounded-xl hover:bg-slate-900 hover:text-white ${sector === "Admin" ? "bg-slate-900 text-white" : "bg-slate-300"}`}
+          onClick={() => setSector("Admin")}
+        >
+          Admin Home Sector
+        </button>
+      </div>
     </div>
   )
 }
